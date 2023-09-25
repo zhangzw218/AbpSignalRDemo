6a595a0b1d37aa74664063b38654ea6cf3a53edb
张志炜 <745787360@qq.com>
2023-09-25 15:39:44 +0800

    update433

---
+++ MigrationDemo/src/MigrationDemo.EntityFrameworkCore.DbMigrations/Migrations/20230925073932_Update433.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MigrationDemo.Migrations
+{
+    public partial class Update433 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
